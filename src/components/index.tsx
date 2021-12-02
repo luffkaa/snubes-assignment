83a600989d5d024baa561bef20c6e53011e2c05d
Luffka <33230841+luffkaa@users.noreply.github.com>
2021-12-02 14:43:50 +0100

    Components added into App, added global styles

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as Consepts } from './Concepts';
+export { default as CopyRights } from './CopyRights';
+export { default as Header} from './Header';
+export { default as Footer } from './Footer';
